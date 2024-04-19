c5b3568476c5613a85bfd6db7d3d51adefafbbd2
David Auk <auk.david@gmail.com>
2024-04-19 16:41:48 +0200

    Make it work

--- server.mjs
+++ server.mjs
@@ -1,24 +1,36 @@
-import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 
-const server = createServer();
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ port: 8081 });
+const leaderboards = []
 
 wss.on('connection', (ws) => {
+  console.log(`${wss.clients.size} clients connected`)
+
   ws.on('error', console.error);
 
   ws.on('message', (data) => {
     const json = JSON.parse(data)
+    if (!json.cmd) return
+    console.log('msg received', json.cmd)
 
-    console.log(json)
-
-    switch (json.request) {
+    switch (json.cmd) {
       case 'ping':
-        const response = JSON.stringify({ uuid: json.uuid, response: 'ping' })
-        ws.send(response)
+        ws.send(JSON.stringify({ response: 'ping' }))
+        break
+
+      case 'listen':
+        leaderboards.push(ws)
+        ws.send(JSON.stringify({ response: 'listen' }))
         break
 
       case 'change':
+        leaderboards.forEach((leaderboard) => {
+          leaderboard.send(JSON.stringify({
+            response: 'notif',
+            uuid: json.uuid,
+            tierlist: json.tierlist,
+          }))
+        })
         break
 
       default:
@@ -26,6 +38,3 @@ wss.on('connection', (ws) => {
     }
   });
 });
-
-console.log('Serving 8081')
-server.listen(8081);
