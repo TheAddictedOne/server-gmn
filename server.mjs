e351f831c9db9e7fdd0c328117acf995d2865d46
David Auk <auk.david@gmail.com>
2024-04-23 22:42:16 +0200

    Save

--- server.mjs
+++ server.mjs
@@ -1,40 +1,43 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({ port: 8081 });
+const wss = new WebSocketServer({ port: 8081 })
 const leaderboards = []
 
 wss.on('connection', (ws) => {
   console.log(`${wss.clients.size} clients connected`)
 
-  ws.on('error', console.error);
+  ws.on('error', console.error)
 
   ws.on('message', (data) => {
     const json = JSON.parse(data)
     if (!json.cmd) return
-    console.log('msg received', json.cmd)
+    console.log(json.cmd)
 
     switch (json.cmd) {
       case 'ping':
         ws.send(JSON.stringify({ response: 'ping' }))
         break
 
-      case 'listen':
+      case 'subscribe':
+        console.log('sub')
         leaderboards.push(ws)
-        ws.send(JSON.stringify({ response: 'listen' }))
+        ws.send(JSON.stringify({ response: 'subscribe' }))
         break
 
       case 'update':
         leaderboards.forEach((leaderboard) => {
-          leaderboard.send(JSON.stringify({
-            response: 'update',
-            uuid: json.uuid,
-            tierList: json.tierList,
-          }))
+          leaderboard.send(
+            JSON.stringify({
+              response: 'player_update',
+              uuid: json.uuid,
+              characters: json.characters,
+            })
+          )
         })
         break
 
       default:
         break
     }
-  });
-});
+  })
+})
